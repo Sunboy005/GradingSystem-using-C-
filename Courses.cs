c40f2d7012f4ec7a965dd4d696cb7034f483f50f
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-10-31 09:19:52 +0100

    Grading System Initial Commit

---
+++ Courses.cs
@@ -0,0 +1,37 @@
+﻿namespace GradingSystem
+{
+    public class Courses
+    {
+        public Courses()
+        {
+
+        }
+        public Courses(int courseId)
+        {
+            this.CourseId = courseId;
+        }
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public string CourseCode { get; set; }
+        public float? CourseUnit { get; set; }
+
+        //Method in Courses Class
+
+        public void Retrieve(int courseId)
+        {
+
+
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+            if (string.IsNullOrWhiteSpace(CourseName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(CourseCode)) isValid = false;
+            if (CourseUnit < 0) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,65 @@
+﻿namespace GradingSystem
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public User(int userId)
+        {
+
+        }
+        public int UserId { get; private set; }
+        public string Dept { get; set; }
+        public string FirstName { get; set; }
+        private string _lastName;
+        public string LastName 
+        { 
+            get 
+            {
+                return _lastName;
+            } 
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName += " ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+        public static int instanceCount { get; set; }
+
+
+
+
+        //Retrieval Method
+        public void Retrieve(int userId)
+        {
+            UserId = userId;            
+        }
+
+        //Validate Method
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrEmpty(LastName)) isValid = false;
+            if (string.IsNullOrEmpty(Dept)) isValid = false;
+            return isValid;
+        }
+
+    }
+}
